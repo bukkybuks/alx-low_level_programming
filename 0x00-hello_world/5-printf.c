1b35b44f793a88dbcf79b41dc2c7631f62fdf991
bukkybuks <bukolafaronbi@gmail.com>
2023-03-18 06:50:43 -0700

    C program with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - This represents that the function takes no argument
+ * Return: 0 (successful execution)
+ */
+int main(void)
+{
+	printf("with proper grammer, the outcome is a piece of art,\n");
+	return (0);
+}
