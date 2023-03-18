b4396ce3c0dd69a13bd3891dc32cd2c067721584
bukkybuks <bukolafaronbi@gmail.com>
2023-03-18 06:38:08 -0700

    C program that prints with a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This represents that the function returns some integer
+ * main (void) - This represents that the function takes no argument
+ * Return: 0 (successful execution)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
