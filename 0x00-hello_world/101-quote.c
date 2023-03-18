af9c9032af8b446c5e7baccbcb3d5339059799d6
bukkybuks <bukolafaronbi@gmail.com>
2023-03-18 08:28:48 -0700

    C program that prints to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+/**
+ * main - This represents that the function takes no argument
+ * Return: 1 (successful execution)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
