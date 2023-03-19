e205ca6d1153c7db5605257d4977b7d764bce65d
bukkybuks <bukolafaronbi@gmail.com>
2023-03-19 08:50:38 -0700

    Printing the alphabet in lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
-		for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 		putchar(alphabet);
 	putchar('\n');
 	return (0);
