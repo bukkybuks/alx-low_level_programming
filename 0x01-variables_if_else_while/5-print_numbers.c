8b6b8956fc42f1e4d83795de91ae0882ca711ac9
bukkybuks <bukolafaronbi@gmail.com>
2023-03-19 09:33:02 -0700

    Printing single digit numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This represents that the function takes no argument
+ * Return: 0 (successful execution)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
