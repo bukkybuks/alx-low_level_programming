2f23f9f10b911dccc775345b0405e6fe4f62866e
bukkybuks <bukolafaronbi@gmail.com>
2023-03-23 11:58:44 -0700

    Prints numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints out the numbers 0 to 9
+ * Return: void (on successful execution)
+ */
+void print_numbers(void)
+{
+	char c = 0;
+
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
