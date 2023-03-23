b84a3af5d87485ab622b53cf90066d70d6e4053e
bukkybuks <bukolafaronbi@gmail.com>
2023-03-23 13:42:01 -0700

    Prints squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i = 1;
+
+		while (i <= size)
+		{
+			int j = 1;
+
+			while (j <= size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
