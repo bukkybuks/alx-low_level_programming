a927ae0055fa1d4e36ccb52a61b3f3397ab118de
bukkybuks <bukolafaronbi@gmail.com>
2023-03-31 10:43:07 -0700

    Changes lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @str: string
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
